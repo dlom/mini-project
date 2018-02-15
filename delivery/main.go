49f661eb876ccfa12c453de56226adb6f834e9cc
Mark Old <dlom234@gmail.com>
2018-02-15 00:37:06 -0800

    Populate `main.go` with a working prototype
    This initial version of `main.go` simply reads the raw values from the
    redis queue and prints them to stdout.  Additionally, the three services
    (ingestion, redis, and delivery) are all capable of interopration with
    each other.

--- delivery/main.go
+++ delivery/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, world!");
+	redisHost := os.Getenv("REDIS_URL")
+	if redisHost == "" {
+		redisHost = "redis://localhost:6379"
+	}
+
+	fmt.Println("Attempting to connect to redis...")
+	c, err := redis.DialURL(redisHost)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, "Failed to connect to redis!")
+		fmt.Fprintln(os.Stderr, "Retrying... (probably)")
+		os.Exit(1)
+	}
+	fmt.Println("Connected!")
+	defer c.Close()
+
+	for {
+		s, err := redis.Strings(c.Do("BLPOP", "queue:postback", 0))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Printf("%#v\n", s[1])
+	}
 }
--- ingestion/ingest.php
+++ ingestion/ingest.php
@@ -4,14 +4,13 @@ require_once "vendor/autoload.php";
 
 header("Content-Type: text/plain");
 
+$redisHost = getenv("REDIS_URL") ?: "redis://localhost:6379";
 $queueName = "queue:postback";
 
 $body = file_get_contents('php://input');
 $json = json_decode($body);
 
-$redis = new Predis\Client([
-    "host" => "redis"
-]);
+$redis = new Predis\Client($redisHost);
 $redis->rpush($queueName, json_encode($json));
 
 http_response_code(204);
