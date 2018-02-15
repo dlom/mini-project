cf9e2acaeaa310fa19eda0eb1d67871d2952d1d8
Mark Old <dlom234@gmail.com>
2018-02-14 20:35:53 -0800

    Populate `ingest.php` with a working prototype
    This initial version of `ingest.php` only contains a barebones
    implementation of the ingestion service.  No error checking or
    validation is performed as of yet.

--- ingestion/ingest.php
+++ ingestion/ingest.php
@@ -1,1 +1,19 @@
-<?php phpinfo(); ?>
+<?php
+
+require_once "vendor/autoload.php";
+
+header("Content-Type: text/plain");
+
+$queueName = "queue:postback";
+
+$body = file_get_contents('php://input');
+$json = json_decode($body);
+
+$redis = new Predis\Client([
+    "host" => "redis"
+]);
+$redis->rpush($queueName, json_encode($json));
+
+http_response_code(204);
+
+?>
