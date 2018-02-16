7a863a1c184a7de23b82a75bd2ca098d2d4e8aeb
Mark Old <dlom234@gmail.com>
2018-02-16 11:35:36 -0800

    Improve the ingestion system
    The ingestion system will now perform several sanity checks on the
    incoming data, which should make the entire set of systems much more
    robust.  With this, the ingestion system is theoretically complete
    barring major architectural changes.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Kochava Mini Project
 ====================
 
-To run: `docker-compose up`
+To run: `docker-compose up -d`
+
+## Rationales
+
+* The entire stringified JSON is pushed into the redis queue because it does not seem to affect performance.  Using the `redis-benchmark` tool, I saw that performance for `POP`ing and `PUSH`ing remained the same from 3 byte payloads all the way up to 3kB payloads.
--- ingestion/ingest.php
+++ ingestion/ingest.php
@@ -7,9 +7,42 @@ header("Content-Type: text/plain");
 $redisHost = getenv("REDIS_URL") ?: "redis://localhost:6379";
 $queueName = getenv("REDIS_QUEUE") ?: "queue:postback";
 
-$body = file_get_contents('php://input');
-$json = json_decode($body);
+// Ensure that the request isn't malformed or malicious
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    http_response_code(405);
+    exit("405 Method Not Allowed");
+}
 
+if ($_SERVER["CONTENT_TYPE"] !== "application/json") {
+    http_response_code(415);
+    exit("400 Bad Request");
+}
+
+// Grab the raw body from the POST request
+$body = file_get_contents("php://input");
+if ($body === false) {
+    http_response_code(500);
+    exit("500 Internal Server Error");
+}
+
+// Decode the body from its JSON representation
+$json = json_decode($body);
+if ($json === NULL) {
+    http_response_code(400);
+    exit("400 Bad Request");
+}
+
+// Perform simple validation on the JSON
+$validator = new JsonSchema\Validator();
+$validator->validate($json, [
+    '$ref' => "file://" . realpath("schema.json")
+]);
+if (!$validator->isValid()) {
+    http_response_code(422);
+    exit("422 Unprocessable Entity");
+}
+
+// Push the encoded JSON to redis
 $redis = new Predis\Client($redisHost);
 $redis->rpush($queueName, json_encode($json));
 
